9f81ba9a2d2e439282fb9bff1d6e9d7d46c4749d
Adam Johnson <adam@adamjohnson.org>
2023-09-01 21:57:59 +1000

    Update Nav.tsx

--- src/components/layout/header/Nav.tsx
+++ src/components/layout/header/Nav.tsx
@@ -39,11 +39,7 @@ function Buttons(props: { setShowingNav: React.Dispatch<React.SetStateAction<boo
           stroke="currentColor"
           className="w-6 h-6"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
-          />
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
       </button>
     </div>
