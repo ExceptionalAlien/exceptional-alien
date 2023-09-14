493f0c0edcf32f4fd4b796ca1f224334b5e99ef9
Adam Johnson <adam@adamjohnson.org>
2023-09-14 17:08:14 +1000

    Sticker header title tweaks

--- src/components/playbook/viewer/list/Header.tsx
+++ src/components/playbook/viewer/list/Header.tsx
@@ -1,6 +1,6 @@
 import { useRef, useState, useEffect } from "react";
 import Image from "next/image";
-import Link from "next/link";
+import Title from "./header/Title";
 import { shimmer, toBase64 } from "@/utils/shimmer";
 
 export default function Header({ data }: { data: any }) {
@@ -24,7 +24,7 @@ export default function Header({ data }: { data: any }) {
   }, []);
 
   return (
-    <div className={`text-white sticky bg-black`} style={{ top: top }} ref={ref}>
+    <div className={`sticky bg-black`} style={{ top: top }} ref={ref}>
       <Image
         src={data.image.url}
         alt={data.image.alt}
@@ -36,27 +36,7 @@ export default function Header({ data }: { data: any }) {
         className="w-full opacity-90"
       />
 
-      <h2 className="w-3/5 !leading-snug md:!leading-tight drop-shadow absolute pl-3 md:pl-4 pb-2 md:pb-3 bottom-0 font-bold text-xl md:text-2xl">
-        {data.title.substring(0, 50)}
-      </h2>
-
-      {/* Creator */}
-      <Link
-        href={"/creators/" + data.creator.uid}
-        className="[&>*]:inline-block drop-shadow p-3 md:p-4 absolute bottom-0 right-0 hover:opacity-50 duration-300 ease-in-out transition-opacity"
-      >
-        <p className="text-sm font-bold max-[320px]:!hidden landscape:!hidden md:landscape:!inline-block">
-          {data.creator.data.first_name} {data.creator.data.last_name.toUpperCase()}
-        </p>
-
-        <Image
-          src={data.creator.data.profile_image.url}
-          alt={data.creator.data.profile_image.alt}
-          width={48}
-          height={48}
-          className="rounded-full ml-2 border-2 border-white"
-        />
-      </Link>
+      <Title data={data} />
     </div>
   );
 }
