98957e53d66176e51cab6b71816a95e0dc1fd37c
Adam Johnson <adam@adamjohnson.org>
2023-10-06 17:41:50 +1100

    Update Category.tsx

--- src/components/gem/heading/Category.tsx
+++ src/components/gem/heading/Category.tsx
@@ -3,12 +3,11 @@ import GemIcon from "@/components/GemIcon";
 
 export default function Heading({ category, destination }: { category: string; destination: string }) {
   return (
-    <Link
-      href={`/destinations/${destination}?filter=${category.toLowerCase()}`}
-      className="hover:opacity-50 duration-300 ease-in-out transition-opacity block"
-    >
+    <Link href={`/destinations/${destination}?filter=${category.toLowerCase()}`} className="group/link block">
       <GemIcon category={category} hideBg={true} classes="box-content [&>svg]:w-10 pt-[11px] pl-2" />
-      <span className="uppercase pl-5 text-ex-blue">{category}</span>
+      <span className="group-hover/link:opacity-50 duration-300 ease-in-out transition-opacity uppercase pl-5 text-ex-blue">
+        {category}
+      </span>
     </Link>
   );
 }
