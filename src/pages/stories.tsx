20bfe5a4b43fbdd8bc31934dde71ca3e0183eae8
Prasath Nadarajah <prasathnadarajah@exceptionalalien.com>
2023-10-31 10:08:33 +1100

    Stories page

---
+++ src/pages/stories.tsx
@@ -0,0 +1,25 @@
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import { createClient } from "@/prismicio";
+
+type PageProps = InferGetStaticPropsType<typeof getStaticProps>;
+export default function Playbooks({ page }: PageProps) {
+  return (
+    <>
+    </>
+  );
+}
+
+export async function getStaticProps({ previewData }: GetStaticPropsContext) {
+  const client = createClient({ previewData });
+
+  const page = await client.getSingle("stories", {
+    fetchLinks:
+      "story.title",
+  });
+
+  return {
+    props: {
+      page,
+    },
+  };
+}
