f799d94711a551a8d66c4c875e9f1cef0b6324e4
Prasath Nadarajah <prasathnadarajah@exceptionalalien.com>
2023-10-31 13:37:52 +1100

    Stories meta data

--- src/pages/stories.tsx
+++ src/pages/stories.tsx
@@ -1,11 +1,31 @@
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import { createClient } from "@/prismicio";
+import Head from "next/head";
 
 type PageProps = InferGetStaticPropsType<typeof getStaticProps>;
 export default function Playbooks({ page }: PageProps) {
   return (
     <>
-      Stories page
+      <Head>
+        <title>{`Exceptional ALIEN - ${page.data.meta_title ? page.data.meta_title : "Stories"}`}</title>
+        <meta name="description" content={page.data.meta_description ?? ""} />
+        <meta property="og:url" content="https://exceptionalalien.com/stories" />
+
+        <meta
+          property="og:title"
+          content={`Exceptional ALIEN - ${page.data.meta_title ? page.data.meta_title : "Stories"}`}
+        />
+
+        <meta property="og:description" content={page.data.meta_description ?? ""} />
+
+        <meta
+          property="og:image"
+          content={page.data.meta_image.url ? page.data.meta_image.url : "https://exceptionalalien.com/img/og.png"}
+        />
+      </Head>
+      <main>
+
+      </main>
     </>
   );
 }
