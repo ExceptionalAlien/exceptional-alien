49882abfee049115f24af6a14b279119d82eff05
Prasath Nadarajah <prasathnadarajah@exceptionalalien.com>
2023-10-31 11:29:53 +1100

    Resolve conflicts

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -38,7 +38,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   const [playbooks, setPlaybooks] = useState<Content.PlaybookDocument<string>[]>([]);
   const [stories, setStories] = useState<Content.StoryDocument<string>[]>([]);
   const [gems, setGems] = useState<Gems>({});
-  const [search, setSearch] = useState<Search>({ destinations: [], playbooks: [], gems: [], creators: [] });
+  const [search, setSearch] = useState<Search>({ stories: [], destinations: [], playbooks: [], gems: [], creators: [] });
 
   return (
     <CreatorsContext.Provider value={{ creators, setCreators }}>
@@ -47,13 +47,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <GemsContext.Provider value={{ gems, setGems }}>
             <SearchContext.Provider value={{ search, setSearch }}>
               <div className={`${helveticaMonospaced.variable} font-mono, ${neueHaasGrotesk.variable} font-sans`}>
-                <div className="p-safe bg-white [&>main>section]:mt-8 [&>main>section]:md:mt-16 [&>main]:m-auto [&>main]:pb-12 [&>main]:pt-12 [&>main]:md:pb-24 [&>main]:md:pt-24">
+                <div className="p-safe bg-white [&>main>section]:mt-8 [&>main>section]:pl-4 [&>main>section]:pr-4 [&>main>section]:md:mt-16 [&>main>section]:md:pl-6 [&>main>section]:md:pr-6 [&>main]:m-auto [&>main]:pb-12 [&>main]:pt-12 [&>main]:md:pb-20 [&>main]:md:pt-20">
                   {children}
                 </div>
 
                 <Footer />
                 <Header />
-                <SearchBox fixed={true} />
               </div>
             </SearchContext.Provider>
           </GemsContext.Provider>
