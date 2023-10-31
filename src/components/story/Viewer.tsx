19ec16bebd638ad5005dba00182d01f792626380
Prasath Nadarajah <prasathnadarajah@exceptionalalien.com>
2023-10-31 16:50:22 +1100

    Stories viewer

---
+++ src/components/story/Viewer.tsx
@@ -0,0 +1,8 @@
+import { Content } from "@prismicio/client";
+
+export default function Viewer({ data }: { data: Content.StoryDocumentData }) {
+  return (
+    <section>
+    </section>
+  );
+}
--- src/pages/stories/[uid].tsx
+++ src/pages/stories/[uid].tsx
@@ -1,12 +1,60 @@
 import { GetStaticPaths, GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import { createClient } from "@/prismicio";
+import Head from "next/head";
+import Viewer from "@/components/story/Viewer";
 
 type PageProps = InferGetStaticPropsType<typeof getStaticProps>;
 
 export default function Playbook({ page }: PageProps) {
   return (
     <>
-      Story page
+      <Head>
+        <title>{`${
+          page.data.meta_title
+            ? page.data.meta_title
+            : `${page.data.title} | ${(page.data.creator as any).data.first_name} ${
+              (page.data.creator as any).data.last_name
+            }`
+        } | Exceptional ALIEN`}</title>
+        <meta name="description" content={page.data.meta_description ?? ""} />
+        <meta property="og:url" content={`https://exceptionalalien.com/travel-playbooks/${page.uid}`} />
+
+        <meta
+          property="og:title"
+          content={`${
+            page.data.meta_title
+              ? page.data.meta_title
+              : `${page.data.title} | ${(page.data.creator as any).data.first_name} ${
+                (page.data.creator as any).data.last_name
+              }`
+          } | Exceptional ALIEN`}
+        />
+
+        <meta
+          property="og:description"
+          content={
+            page.data.meta_description
+              ? page.data.meta_description
+              : page.data.title
+                ? page.data.title
+                : ""
+          }
+        />
+
+        <meta
+          property="og:image"
+          content={
+            page.data.meta_image.url
+              ? page.data.meta_image.url
+              : page.data.image.url
+                ? page.data.image.url
+                : "https://exceptionalalien.com/img/og.png"
+          }
+        />
+      </Head>
+      <main className="!pb-0">
+        <Viewer data={page.data} />
+      </main>
     </>
   );
 }
@@ -24,7 +72,7 @@ export async function getStaticProps({ params, previewData }: GetStaticPropsCont
 
     const page = await client.getByUID("story", params?.uid as string, {
       fetchLinks:
-        "story.title",
+        "story.title,creator.first_name,creator.last_name,creator.profile_image,creator.uid,destination.title",
     });
 
     return {
