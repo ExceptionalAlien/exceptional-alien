3c335cf682c25cfbf29aa4c75575b2b2e51d5d1c
Adam Johnson <adam@adamjohnson.org>
2023-11-03 17:39:59 +1100

    Gem slice QA

--- src/components/TabButton.tsx
+++ src/components/TabButton.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
 
-export default function TabButton({ text, route, classes }: { text: string; route: string; classes?: string }) {
+interface TabButtonProps {
+  text: string;
+  route: string;
+  classes?: string;
+}
+
+export default function TabButton(props: TabButtonProps) {
   return (
     <Link
-      href={route}
-      className={`relative block text-sm p-2 pt-1 pb-0 border border-b-0 border-black hover:border-ex-blue hover:text-ex-blue transition-[border-color,color] duration-300 ease-in-out ${classes}`}
+      href={props.route}
+      className={`relative block border border-b-0 border-black p-2 pb-0 pt-1 text-sm transition-[border-color,color] duration-300 ease-in-out hover:border-ex-blue hover:text-ex-blue ${props.classes}`}
     >
-      {text}
+      {props.text}
       <p className="absolute right-2 top-0 text-lg">{"\u2794"}</p>
     </Link>
   );
--- src/slices/Gem/index.tsx
+++ src/slices/Gem/index.tsx
@@ -4,6 +4,7 @@ import { Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import TabButton from "@/components/TabButton";
 import GemIcon from "@/components/shared/GemIcon";
+import CreatorIcon from "@/components/shared/CreatorIcon";
 import { shimmer, toBase64 } from "@/utils/shimmer";
 
 /**
@@ -30,10 +31,10 @@ const Gem = ({ slice, context }: GemProps): JSX.Element => {
       className="gem relative"
       id={`gem-${gem.uid}`}
     >
-      <GemIcon category={gem.data.category} classes="right-0 !m-0" />
+      <GemIcon category={gem.data.category} classes="right-0" />
 
-      <hgroup className="mr-10 md:mr-12 [&>*]:leading-snug">
-        <h4 className="font-bold text-xl md:text-2xl">{gem.data.title}</h4>
+      <hgroup className="mr-10 pr-1 md:mr-12 md:pr-2 [&>*]:leading-snug">
+        <h4 className="text-xl font-bold md:text-2xl">{gem.data.title}</h4>
 
         <h5>
           {gem.data.description?.charAt(0).toUpperCase()}
@@ -45,15 +46,10 @@ const Gem = ({ slice, context }: GemProps): JSX.Element => {
       <a
         href={`https://www.google.com/maps/search/?api=1&query=${gem.data.title}&query_place_id=${gem.data.google_maps_id}`}
         target="_blank"
-        className="text-ex-grey text-sm md:text-base hover:text-ex-light-grey duration-300 ease-in-out transition-[color] inline-block mr-10 md:mr-12"
+        className="mr-10 inline-flex items-center pr-1 text-sm text-ex-grey transition-[color] duration-300 ease-in-out hover:text-ex-light-grey md:mr-12 md:pr-2 md:text-base"
       >
         {gem.data.address}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="w-4 h-4 inline-block ml-1 align-[-2px]"
-        >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="ml-1 h-4 w-4">
           <path
             fillRule="evenodd"
             d="M12.207 2.232a.75.75 0 00.025 1.06l4.146 3.958H6.375a5.375 5.375 0 000 10.75H9.25a.75.75 0 000-1.5H6.375a3.875 3.875 0 010-7.75h10.003l-4.146 3.957a.75.75 0 001.036 1.085l5.5-5.25a.75.75 0 000-1.085l-5.5-5.25a.75.75 0 00-1.06.025z"
@@ -63,56 +59,47 @@ const Gem = ({ slice, context }: GemProps): JSX.Element => {
       </a>
 
       {/* Details */}
-      <div className="gem-content mt-2 md:mt-3">
+      <div className="gem-content mt-1 md:mt-2">
         {gem.data.image && (
           <Image
             src={gem.data.image.thumb.url as string}
             alt={gem.data.image.thumb.alt ? (gem.data.image.thumb.alt as string) : (gem.data.title as string)}
-            width={gem.data.image.thumb.dimensions!.width}
-            height={gem.data.image.thumb.dimensions!.height}
+            width={gem.data.image.thumb.dimensions?.width}
+            height={gem.data.image.thumb.dimensions?.height}
             placeholder={`data:image/svg+xml;base64,${toBase64(
-              shimmer(gem.data.image.thumb.dimensions!.width, gem.data.image.thumb.dimensions!.height)
+              shimmer(
+                gem.data.image.thumb.dimensions?.width as number,
+                gem.data.image.thumb.dimensions?.height as number
+              )
             )}`}
-            className="relative w-2/5 float-left"
+            className="float-left w-2/5"
           />
         )}
 
-        <div className="float-left pb-6 mb-6 md:mb-8 pl-3 md:pl-4 w-3/5 [&>*]:text-ex-blue [&>*]:text-sm [&>*]:md:text-base [&>p]:font-bold">
+        <div className="float-left mb-6 w-3/5 pb-[25px] pl-2 md:mb-9 md:pl-3 [&>p]:text-sm [&>p]:font-bold [&>p]:text-ex-blue [&>p]:md:text-base">
           <PrismicRichText field={slice.primary.description} />
 
           {!creator.data ? (
-            <p className="mt-3 md:mt-4 !font-normal">
+            <p className="mt-2 !font-normal md:mt-3">
               {context.creator.data.first_name} {context.creator.data.last_name?.toUpperCase()}
             </p>
           ) : (
             <Link
-              href={`/creators/${creator.uid}`}
-              className="group/link block max-w-full w-max mt-3 md:mt-4 hover:text-ex-light-grey duration-300 ease-in-out transition-[color]"
+              href={`/community/${creator.uid}`}
+              className="mt-2 block transition-[opacity] duration-300 ease-in-out hover:opacity-60 md:mt-3"
             >
-              {creator.data.first_name} {creator.data.last_name?.toUpperCase()}
-              <Image
-                src={creator.data.profile_image.url as string}
-                alt={
-                  creator.data.profile_image.alt
-                    ? (creator.data.profile_image.alt as string)
-                    : creator.data.last_name
-                    ? `${creator.data.first_name} ${creator.data.last_name}`
-                    : (creator.data.first_name as string)
-                }
-                width={40}
-                height={40}
-                className={`inline-block rounded-full ml-2 w-8 md:w-10 align-[-11px] md:align-[-14px] group-hover/link:opacity-50 duration-300 ease-in-out transition-[opacity] ${
-                  !creator.data.last_name && "hidden"
-                }`}
+              <CreatorIcon
+                firstName={creator.data.first_name as string}
+                lastName={creator.data.last_name as string}
+                image={creator.data.profile_image}
+                classes="[&>p]:text-ex-blue [&>img]:h-10 [&>img]:w-10"
               />
             </Link>
           )}
         </div>
 
-        <div className="clear-both"></div>
-
-        <div className="w-3/5 !absolute bottom-0 right-0">
-          <TabButton text="MORE INFO" route={"/gems/" + gem.uid} classes="ml-3 md:ml-4" />
+        <div className="!absolute bottom-0 right-0 w-3/5">
+          <TabButton text="MORE INFO" route={"/gems/" + gem.uid} classes="ml-2 md:ml-3" />
         </div>
       </div>
     </section>
