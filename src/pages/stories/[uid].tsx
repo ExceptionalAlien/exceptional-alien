b3360a08ce18413764273bb97ba13ed863fbc1cd
Prasath Nadarajah <prasathnadarajah@exceptionalalien.com>
2023-11-06 11:54:44 +1100

    Add meta details

--- src/pages/stories/[uid].tsx
+++ src/pages/stories/[uid].tsx
@@ -1,11 +1,43 @@
 import { GetStaticPaths, GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import { createClient } from "@/prismicio";
+import Head from "next/head";
 
 type PageProps = InferGetStaticPropsType<typeof getStaticProps>;
 
 export default function Story({ page }: PageProps) {
   return (
     <>
+      <Head>
+        <title>{`${page.data.meta_title ? page.data.meta_title : page.data.title} | Exceptional ALIEN`}</title>
+
+        <meta
+          name="description"
+          content={page.data.meta_description ? page.data.meta_description : page.data.title as string}
+        />
+
+        <meta property="og:url" content={`https://exceptionalalien.com/stories/${page.uid}`} />
+
+        <meta
+          property="og:title"
+          content={`${page.data.meta_title ? page.data.meta_title : page.data.title} | Exceptional ALIEN`}
+        />
+
+        <meta
+          property="og:description"
+          content={page.data.meta_description ? page.data.meta_description : page.data.title as string}
+        />
+
+        <meta
+          property="og:image"
+          content={
+            page.data.meta_image.url
+              ? page.data.meta_image.url
+              : page.data.image.url
+                ? page.data.image.url
+                : "https://exceptionalalien.com/img/og.png"
+          }
+        />
+      </Head>
     </>
   );
 }
