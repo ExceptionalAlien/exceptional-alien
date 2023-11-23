5f68d7b0bef9a3a32872d0af6ef7ecbd8bc1a525
Adam Johnson <adam@adamjohnson.org>
2023-11-23 18:35:27 +1100

    Small changes

--- src/components/playbooks/Featured.tsx
+++ src/components/playbooks/Featured.tsx
@@ -9,7 +9,7 @@ interface FeaturedProps {
 export default function Featured(props: FeaturedProps) {
   return (
     <section className="!pl-0 !pr-0">
-      <h3 className="p-4 !pt-0 pb-2 text-2xl font-bold md:p-6 md:pb-3 md:text-4xl">Featured Travel Playbooks</h3>
+      <h3 className="p-4 !pt-0 pb-2 text-2xl font-bold md:p-6 md:pb-3 md:text-4xl">Latest Travel Playbooks</h3>
 
       <Slider>
         {props.playbooks.map((item, i) => (
--- src/components/shared/GemThumb.tsx
+++ src/components/shared/GemThumb.tsx
@@ -44,7 +44,7 @@ export default function GemThumb(props: GemThumbProps) {
 
           <GemIcon
             category={props.gem.data.category}
-            classes={`top-0 m-[6px] md:m-[10px] ${props.size === "med" && "!h-12 !w-12"}`}
+            classes={`top-0 m-[6px] md:m-[10px] ${props.size === "med" && "!h-10 !w-10 md:!h-12 md:!w-12"}`}
           />
 
           {/* Creators */}
