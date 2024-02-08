bd1496c5dc60a673cca6ab23670d8c68551f08f8
Adam Johnson <adam@adamjohnson.org>
2024-02-08 21:55:59 +1100

    Update route.ts

--- src/app/api/playbooks/[destination]/route.ts
+++ src/app/api/playbooks/[destination]/route.ts
@@ -3,8 +3,7 @@ import { createClient } from "@/prismicio";
 export async function GET(request: Request, { params }: { params: { destination: string } }) {
   const client = createClient();
   const destination = await client.getByUID("destination", params.destination, {
-    fetchLinks:
-      "playbook.title,playbook.image,playbook.creator,creator.first_name,creator.last_name,creator.profile_image",
+    fetchLinks: "playbook.title,playbook.image,playbook.creator,creator.first_name,creator.last_name,creator.title",
   });
   return Response.json(destination.data.featured);
 }
