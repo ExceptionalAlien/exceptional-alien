0ff397b0192be62906e3cb93518e258be61e4003
Adam Johnson <adam@adamjohnson.org>
2024-03-25 15:15:23 +1100

    Create route.ts

---
+++ src/app/api/gem/[uid]/route.ts
@@ -0,0 +1,16 @@
+import { createClient } from "@/prismicio";
+
+export async function GET(request: Request, { params }: { params: { uid: string } }) {
+  const client = createClient();
+
+  const playbook = await client.getByUID("playbook", params.uid, {
+    fetchOptions: {
+      cache: "no-store",
+    },
+    fetch: "gem.title,gem.category,gem.playbooks,gem.description,gem.address,gem.image,gem.about.gem.website",
+    fetchLinks:
+      "playbook.sub_title,playbook.creator,playbook.app_title,playbook.destination,playbook.slices,playbook.locked,playbook.image,creator.profile_image,creator.first_name,creator.last_name,creator.title,destination.title",
+  });
+
+  return Response.json(playbook);
+}
