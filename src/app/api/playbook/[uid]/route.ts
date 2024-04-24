d07e91fb35972e2e56a1ac3f4d7d0d8ebddc05ae
Adam Johnson <adam@adamjohnson.org>
2024-04-24 21:58:57 +1000

    Update route.ts

--- src/app/api/playbook/[uid]/route.ts
+++ src/app/api/playbook/[uid]/route.ts
@@ -10,7 +10,7 @@ export async function GET(request: Request, { params }: { params: { uid: string 
     fetch:
       "playbook.description,playbook.slices,playbook.sub_title,playbook.app_title,playbook.image,playbook.locked,playbook.creator,playbook.destination",
     fetchLinks:
-      "creator.first_name,creator.last_name,creator.title,creator.profile_image,destination.title,gem.title,gem.category,gem.description,gem.address,gem.playbooks,gem.location,gem.image,gem.destination,playbook.sub_title,playbook.creator,playbook.app_title,playbook.destination,playbook.slices,playbook.locked,playbook.image",
+      "creator.first_name,creator.last_name,creator.title,creator.profile_image,destination.title,gem.title,gem.category,gem.description,gem.address,gem.location,gem.image,gem.destination",
   });
 
   return Response.json(playbook);
